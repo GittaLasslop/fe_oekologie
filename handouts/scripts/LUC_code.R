2749a3d5c3fd1ec3abc76eab4cd591a6a71ecd30
GittaLasslop <gitta.lasslop@gmail.com>
2019-06-06 16:45:37 +0200

    updated codes

--- handouts/scripts/LUC_code.R
+++ handouts/scripts/LUC_code.R
@@ -1,212 +1,190 @@
+install.packages("caret")
 library(raster)
 library(ggplot2)
 library(reshape2)
-library(FEglobaleOekologie)
+library(caret)
 
-fegloekOptions(baseDir = "/Users/cwerner/Dropbox/Documents/2018/teaching/fe_oekologie_2018/handouts")
-fegloekOptions(region="NWBRAZIL")
-fegloekInit()
+setwd('S:/FGOE/')
 
-# Lesefunktion aus den letzten Sitzungen
-#
-
-geotiff2df <- function(file, name="value", valid_range=NA, crop_extent=NA, scale_factor=NA) {
- rast <- raster(file)
-
- ## AUFGABE:
- ## > Beschneide das GeoTiff mit crop (falls crop nicht NA ist)
-
- ## HINWEIS:
- ## Wir benoetigen (1) ein zusaetzliches Funktionsargument 'crop_extent' mit dem Standardwert
- ## NA (nicht definiert) 
- ## Weiterhin benoetigen wir (2) den Befehl crop (?crop) um die Ausdehnung unserer Karte
- ## auf die Grenzen 'crop_extent' zu beschneiden.
-
- ## Fuer den Aufbau der if-Bedingung koennen Sie sich bei der folgenden Abfrage von
- ## 'valid_range' inspirieren lassen
-
-
- ## NEUEN CODE HIER EINSETZEN
- if (!any(is.na(crop_extent))) {
-  rast <- crop(rast, crop_extent)
- }
- ## ENDE FUER NEUEN CODE
-
-
- ## Setze valid range und scale factor (falls gegeben)
- if (!any(is.na(valid_range))) {
-   rast[rast <= valid_range[1]] = NA
-   rast[rast >= valid_range[2]] = NA
- }
- if (!is.na(scale_factor))
-   rast = rast * scale_factor
-
- ## konvertiere in einen data.frame
- df <- as.data.frame(rasterToPoints(rast))
- colnames(df) <- c("x", "y", name)
- return(df)
+geotiff2df <- function(file, name="value", valid_range=NA, scale_factor=NA, crop_extent=NA) {
+  rast <- raster(file)
+  
+  ## beschneide das GeoTiff mit crop_extent (falls crop_extent nicht NA ist)
+  ## crop muss folgendes Format haben: c(xmin, xmax, ymin, ymax)
+  if(!is.na(crop_extent)){
+    rast=crop(rast,extent(crop_extent))
+  }
+  ## Setze valid range und scale factor (falls gegeben)
+  if (!is.na(valid_range)) {
+    rast[rast < valid_range[1]] = NA
+    rast[rast > valid_range[2]] = NA
+  }
+  if (!is.na(scale_factor)){
+    rast = rast * scale_factor
+  }
+  ## konvertiere in einen data.frame
+  df <- as.data.frame(rasterToPoints(rast))
+  colnames(df) <- c("x", "y", name)
+  return(df)
 }
-
-
-
-
-# Erinnerung: die Daten liegen auf
-# file='https://powerfolder.gwdg.de/filestable/MjRGYXU2YUViNThFdU5yMlFhZXV2'
-# bitte herunterladen und in data Verzeichnis entzippen
-
-
-## (1) Karten
-
-fpath <- file.path(getOption("fegloekDataDir"),getOption("fegloekRegion"))
-message(fpath)
-
 
 start_year <- 2001
 end_year <- 2010
 
-forestExtent <- getOption("fegloekLUCExtent")
-data.dir <- file.path(getOption("fegloekDataDir"), getOption("fegloekRegion"))
+forestExtent = c(1000000, 1500000, 8500000, 9000000)
+data.dir = 'Pfad zu ihrem Datenordner'
 for (year in start_year:end_year) {
   file <- paste0("MCD12Q1_", year ,"-01-01.Land_Cover_Type_3.tif")
-  dfLCT <- geotiff2df(file.path(data.dir, file), name="id", crop_extent=forestExtent, valid_range = c(0, 253))
-
-  # reproject border lines, if not yet done
-  if (!exists("regionmap")) {
-    rLCT <- raster(file.path(data.dir, file))
-    rLCT = crop(rLCT, forestExtent)
-    regionmap = borderSubset(rLCT)
-  }
-
-  dfLCT$name=""
+  dfLCT <- geotiff2df(file.path(data.dir, file), name="id", 
+                      crop_extent =forestExtent, valid_range = c(0, 253))
+  
+  dfLCT$name = ""
   dfLCT$name[dfLCT$id < 5 | dfLCT$id > 8] = "unforested"
   dfLCT$name[dfLCT$id > 4 & dfLCT$id < 9] = "forested"
   dfLCT$name[dfLCT$id == 0] = "water"
-
+  
   nFor = length(dfLCT$name[dfLCT$name == "forested"])
   nUnf = length(dfLCT$name[dfLCT$name == "unforested"])
-
+  
   if (year == start_year) {
     tsForest = data.frame(Year=year, forested=nFor, unforested=nUnf)
   } else {
     tsForest = rbind(tsForest, data.frame(Year=year, forested=nFor, unforested=nUnf))
   }
-
+  
   if (year == start_year || year == end_year) {
     p <- ggplot(dfLCT, aes(x=x, y=y))
     p <- p + geom_raster(aes(fill=name))
-    p <- p + geom_path(data=regionmap, size=0.1, colour="black", aes(x=long, y=lat, group=group))
     p <- p + coord_fixed(xlim=c(min(dfLCT$x), max(dfLCT$x)),
                          ylim=c(min(dfLCT$y), max(dfLCT$y)))
     p <- p + theme(legend.position = "bottom")
     p <- p + scale_fill_manual(values=c("unforested"="brown", "forested"="darkgreen",
                                         "water"="lightblue"), drop=FALSE)
     p <- p + guides(fill = guide_legend(title=NULL, ncol = 4))
-    ##label = paste0(nFor, " * km^2")
-    ##p <- p + annotate("text", label=label, x=3e5, y=9.05e6,
-    ##                  color="white", parse=TRUE, size=6)
-    ##label = paste0(nUnf, " * km^2")
-    ##p <- p + annotate("text", label=label, x=3e5, y=8.95e6,
-    ##                  color="white", parse=TRUE, size=6)
     p <- p + labs(title=year)
     p <- p + xlab("easting")
     p <- p + ylab("northing")
-    file <- paste0(getOption("fegloekRegion"), "_LUC_", year, ".pdf")
-    pdf(file.path(getOption("fegloekFigDir"), file), paper="special", width=10, height=6)
+    file <- paste0("NWBRAZIL_LUC_", year, ".pdf")
+#    pdf(file.path("Pfad zu ihrem Plot Ordner", file))
     print(p)
-    dev.off()
+#    dev.off()
   }
 }
 
-
-
-## (2) Zeitreihe
 tsForest = melt(tsForest, measure.vars=c("forested", "unforested"))
 p <- ggplot(tsForest, aes(x=Year, y=value, fill=variable))
 p <- p + geom_bar(stat="identity", position="dodge")
 p <- p + scale_fill_manual(values=c("unforested"="brown", "forested"="darkgreen",
-                                        "water"="lightblue"), drop=FALSE)
+                                    "water"="lightblue"), drop=FALSE)
 
-## p <- p + facet_wrap(~variable, ncol=1, scales="free_y")
-
-file <- paste0(getOption("fegloekRegion"), "_LUC_TS.pdf")
-pdf(file.path(getOption("fegloekFigDir"), file), paper="special", width=10, height=6)
+file <- paste0("BRAZIL_LUC_TS.pdf")
+pdf(file.path('plots/LUC/', file), paper="special", width=10, height=6)
 print(p)
 dev.off()
 
+forestExtent = c(1000000, 1500000, 8500000, 9000000)
 
-## (3) Animation
+year <- 2001
+file <- paste0("MCD12Q1_", year ,"-01-01.Land_Cover_Type_3.tif")
+dfLCT1 <- geotiff2df(file.path(data.dir, file), name="id", 
+                     crop_extent =forestExtent, valid_range = c(0, 253))
 
-## Installation des Zusatzpakets
-library(devtools)
-devtools::install_github("dgrtwo/gganimate")
-## laden des neuen Pakets
-library(gganimate)
+idx.forest.2001 <- which(dfLCT1$id > 4 & dfLCT1$id < 9)
 
+year <- 2010
+file <- paste0("MCD12Q1_", year ,"-01-01.Land_Cover_Type_3.tif")
+dfLCT2 <- geotiff2df(file.path(data.dir, file), name="id", 
+                     crop_extent =forestExtent, valid_range = c(0, 253))
+idx.noforest.2010 <- which(dfLCT2$id < 5 | dfLCT2$id > 8)
 
-forestExtent <- getOption("fegloekLUCExtent")
-data.dir <- file.path(getOption("fegloekDataDir"), getOption("fegloekRegion"))
+idx.change <- intersect(idx.forest.2001, idx.noforest.2010)
 
-list_of_dfLCTs = list()
-cnt = 1
+dfLCT.change <- dfLCT2[idx.change, ]
 
-for (year in start_year:end_year) {
-  print(year)
+file <- 'LCT3Lookuptable.txt'
+LCT3lookuptable=read.table(file.path(data.dir, file),header=T)
+dfLCT.change = merge(dfLCT.change, LCT3lookuptable, by="id", all.x=TRUE)
 
-  file <- paste0("MCD12Q1_", year ,"-01-01.Land_Cover_Type_3.tif")
-  dfLCT <- geotiff2df(file.path(data.dir, file), name="id", crop=forestExtent, valid_range = c(0, 253))
+table(dfLCT.change$name)
 
-  # reproject border lines, if not yet done
-  if (!exists("regionmap")) {
-    rLCT <- raster(file.path(data.dir, file))
-    rLCT = crop(rLCT, forestExtent)
-    regionmap = borderSubset(rLCT)
-  }
+df.change <- as.data.frame(table(dfLCT.change$name))
+colnames(df.change) <- c("Class","Freq")
+df.change <- df.change[which(df.change$Freq!=0),]
+df.change
 
-  dfLCT$name=""
-  dfLCT$name[dfLCT$id < 5 | dfLCT$id > 8] = "unforested"
-  dfLCT$name[dfLCT$id > 4 & dfLCT$id < 9] = "forested"
-  dfLCT$name[dfLCT$id == 0] = "water"
+p <- ggplot(df.change, aes(x=Class, y = Freq, fill=Class))
+p <- p + geom_bar(width = 1, stat = "identity", color = "black")
+p <- p + coord_flip()
+print(p)
 
-  dfLCT$year=year
-  
-  list_of_dfLCTs[[cnt]] = dfLCT
-  cnt = cnt + 1
+forestExtent = c(1000000, 1500000, 8500000, 9000000)
+year <- 2010
+file <- paste0("MCD12Q1_", year ,"-01-01.Land_Cover_Type_3.tif")
+modis <- raster(file.path(data.dir, file))
+modis <- crop(modis, forestExtent)
+
+valid_range <- c(0, 253)
+if (!any(is.na(valid_range))) {
+  modis[modis <= valid_range[1]] = NA
+  modis[modis >= valid_range[2]] = NA
 }
 
-## Verknuepfung der einzelnen dfLCT data.frames zu einem grossen data.frame
-dfLCT_total <- do.call("rbind", list_of_dfLCTs)
+projection(modis)
+#[1] "+proj=utm +zone=20 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
-## Transformation mit melt
-#print(head(dfLCT_total))
-#dfLCT_total_melted <- melt(dfLCT_total, measure.vars=c("name"))
+file <- "ESACCI-LC-L4-LCCS-Map-300m-2010-Subset.tif"
+esa <- raster(file.path(data.dir, file))
+projection(esa)
 
-#print(head(dfLCT_total_melted))
+Mode <- function(x, ...) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+esa.agg <- aggregate(esa, fact=4, fun=Mode, expand=TRUE, na.rm=TRUE)
 
-p <- ggplot(dfLCT_total, aes(x=x, y=y, frame=year))
-p <- p + geom_raster(aes(fill=name))
-p <- p + geom_path(data=regionmap, size=0.1, colour="black", aes(x=long, y=lat, group=group))
-p <- p + coord_fixed(xlim=c(min(dfLCT$x), max(dfLCT$x)),
-                         ylim=c(min(dfLCT$y), max(dfLCT$y)))
-p <- p + theme(legend.position = "bottom")
-p <- p + scale_fill_manual(values=c("unforested"="brown", "forested"="darkgreen",
-                                        "water"="lightblue"), drop=FALSE)
-p <- p + guides(fill = guide_legend(title=NULL, ncol = 4))
+modis.longlat <- projectRaster(from=modis, to=esa.agg, method="ngb") #nearest neighbor
 
+esa.agg.reclass <- esa.agg
+esa.agg.reclass[esa.agg.reclass == 0] <- 255
+esa.agg.reclass[esa.agg.reclass == 10] <- 1
+esa.agg.reclass[esa.agg.reclass == 11] <- 1
+esa.agg.reclass[esa.agg.reclass == 12] <- 3
+esa.agg.reclass[esa.agg.reclass == 20] <- 1
+esa.agg.reclass[esa.agg.reclass == 30] <- 4
+esa.agg.reclass[esa.agg.reclass == 40] <- 4
+esa.agg.reclass[esa.agg.reclass == 50] <- 5
+esa.agg.reclass[esa.agg.reclass == 60] <- 6
+esa.agg.reclass[esa.agg.reclass == 61] <- 6
+esa.agg.reclass[esa.agg.reclass == 62] <- 6
+esa.agg.reclass[esa.agg.reclass == 70] <- 7
+esa.agg.reclass[esa.agg.reclass == 71] <- 7
+esa.agg.reclass[esa.agg.reclass == 72] <- 7
+esa.agg.reclass[esa.agg.reclass == 80] <- 8
+esa.agg.reclass[esa.agg.reclass == 81] <- 8
+esa.agg.reclass[esa.agg.reclass == 82] <- 8
+esa.agg.reclass[esa.agg.reclass == 90] <- 5
+esa.agg.reclass[esa.agg.reclass == 100] <- 4
+esa.agg.reclass[esa.agg.reclass == 110] <- 4
+esa.agg.reclass[esa.agg.reclass == 120] <- 2
+esa.agg.reclass[esa.agg.reclass == 121] <- 2
+esa.agg.reclass[esa.agg.reclass == 122] <- 2
+esa.agg.reclass[esa.agg.reclass == 130] <- 1
+esa.agg.reclass[esa.agg.reclass == 140] <- 1
+esa.agg.reclass[esa.agg.reclass == 150] <- 9
+esa.agg.reclass[esa.agg.reclass == 152] <- 9
+esa.agg.reclass[esa.agg.reclass == 153] <- 9
+esa.agg.reclass[esa.agg.reclass == 160] <- 5
+esa.agg.reclass[esa.agg.reclass == 170] <- 5
+esa.agg.reclass[esa.agg.reclass == 180] <- 2
+esa.agg.reclass[esa.agg.reclass == 190] <- 10
+esa.agg.reclass[esa.agg.reclass == 200] <- 9
+esa.agg.reclass[esa.agg.reclass == 201] <- 9
+esa.agg.reclass[esa.agg.reclass == 202] <- 9
+esa.agg.reclass[esa.agg.reclass == 210] <- 0
+esa.agg.reclass[esa.agg.reclass == 220] <- 255
 
-## label = paste0(nFor, " * km^2")
-## p <- p + annotate("text", label=label, x=3e5, y=9.05e6,
-##                  color="white", parse=TRUE, size=12)
-## label = paste0(nUnf, " * km^2")
-## p <- p + annotate("text", label=label, x=3e5, y=8.95e6,
-##                  color="white", parse=TRUE, size=12)
-    
-#p <- p + labs(title=year)
-p <- p + xlab("easting")
-p <- p + ylab("northing")
+library(caret)
 
-gganimate(p, filename="meine_animation.html")
-
-## falls sie die animation in ein GIF speicher wollen (sie benötigen ImageMagick dafuer)
-## gganimate(p, "animation.gif")
-
-
+modis.factor <- factor(values(modis.longlat), levels = c(0,1,2,3,4,5,6,7,8,9,10))
+esa.factor <- factor(values(esa.agg.reclass), levels = c(0,1,2,3,4,5,6,7,8,9,10))
+confusionMatrix(data=modis.factor, reference=esa.factor, dnn = c("MODIS", "ESA"))
--- handouts/scripts/NPP_de.R
+++ handouts/scripts/NPP_de.R
@@ -1,56 +1,67 @@
-library(raster)
-library(ggplot2)
-library(plyr)
-library(reshape2)
-library(FEglobaleOekologie)
-fegloekOptions(baseDir = "/hier/muss/ihr/pfad/stehen")
-fegloekOptions(region = "BORNEO")
-fegloekInit()
+# R commands zum handout NPP
 
-# Erinnerung: die Daten liegen auf
-# file='https://powerfolder.gwdg.de/filestable/MjRGYXU2YUViNThFdU5yMlFhZXV2'
-# bitte herunterladen und in data Verzeichnis entzippen
+for (p in c("raster", "rgeos", "ggplot2", "rworldmap", "maps", 
+            "mapproj", "rgdal")) {
+  if (!require(p, character.only=TRUE)) {
+    install.packages(p)
+    library(p, character.only=TRUE)
+  }
+}
 
+setwd('/home/gitta/R/Lehre/FE_OEKOLOGIE/fe_oekologie_2019/handouts/')
 
-# wie zuvor suchen Sie bitte dir richtigen Faktoren heraus
+geotiff2df <- function(file, name, valid_range, scale_factor=1){
+  rdata=raster(file)
+  rdata[rdata<valid_range[1]]=NA
+  rdata[rdata>valid_range[2]]=NA
+  rdata=rdata*scale_factor
+  
+  dfdata=as.data.frame(rasterToPoints(rdata))
+  names(dfdata)=c('x','y',name)
+  
+  return(dfdata)
+}
 
-data.dir <- file.path(getOption("fegloekDataDir"), getOption("fegloekRegion"))
+nppScaleFactor <- 0.0001
+nppValidRange  <- c(0, 65500)
+
+data.dir <- 'data/NPP/MEXICO'
 file <- file.path(data.dir, "MOD17A3_2010-01-01.Npp_1km.tif")
 dfNPP <- geotiff2df(file, "NPP", valid_range = nppValidRange, 
                     scale_factor = nppScaleFactor)
 
+p=ggplot(data=dfNPP,aes(x = x, y = y))
+p = p + geom_raster(aes(fill = NPP))
+p = p + scale_fill_gradient(low = "yellow", high = "green")
+p = p + coord_fixed()
+print(p)
 
-# Die Variable LCT3lookuptable muss wieder eingelesen werden (externe txt Datei)
+LatNPP=aggregate(dfNPP[3],by=list(lat=dfNPP$y),FUN='mean')
 
+plot(LatNPP$lat,LatNPP$NPP,xlab='Latitude', ylab='NPP [kg C/m²/Jahr]',type='l',lwd=3)
+
+LCTlookuptable=read.table('data/NPP/MEXICO/LCT3Lookuptable.txt',header=TRUE)
 file <-file.path(data.dir, "MCD12Q1_2010-01-01.Land_Cover_Type_3.tif")
 dfLCT <- geotiff2df(file, 'id', valid_range = c(0, 253))
-dfLCT = merge(dfLCT, LCT3lookuptable, by="id", all.x=TRUE)
+dfLCT = merge(dfLCT, LCTlookuptable, by="id", all.x=TRUE)
 
-
-if (getOption("fegloekRegion") == "ALASKA" ||
-    getOption("fegloekRegion") == "SAFRICA" ||
-    getOption("fegloekRegion") == "SWAUSTRALIA" ||
-    getOption("fegloekRegion") == "SEAUSTRALIA") {
-    dfNPP$x = round(dfNPP$x, -3)
-    dfNPP$y = round(dfNPP$y, -3)
-    dfLCT$x = round(dfLCT$x, -3)
-    dfLCT$y = round(dfLCT$y, -3)
-}
+p <- ggplot(dfLCT, aes(x = x, y = y))
+p = p + geom_raster(aes(fill = name))
+p = p + coord_fixed()
+print(p)
 
 dfData <- merge(dfNPP, dfLCT, by=c("x", "y"))
-
-# Plotting
-
+ind=which(dfData$name=='Non-vegetated'|dfData$name=='Deciduous Needleleaf forest'|dfData$name=='Water')
+dfData=dfData[-ind,]
 p <- ggplot(dfData, aes(x=name, y=NPP, fill=name))
-p <- p + geom_boxplot()
+p <- p + geom_boxplot(na.rm=T)
 p <- p + guides(fill=FALSE)
 p <- p + xlab(NULL)
 p <- p + ylab("NPP [kg(C)/m^2]")
 p <- p + coord_flip()
-```
-```{r, eval=FALSE}
-file <- paste0(getOption("fegloekRegion"), "_NPP.pdf")
-pdf(file.path(getOption("fegloekFigDir"), file), paper="special", width=8, height=8)
+p
+
+file <- paste0("plots/NPP/Borneo_NPP.pdf")
+pdf( file, paper="special", width=8, height=8)
 print(p)
 dev.off()
-
